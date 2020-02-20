ff861b35f5e0713c11a635c9444f58ef8ba681f5
Aawaz Gyawali <awazgyawali@gmail.com>
2020-02-20 09:55:25 +0545

    Iitilized dart layer

--- lib/native_http.dart
+++ lib/native_http.dart
@@ -1,13 +1,42 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/services.dart';
 
-class NativeHttp {
-  static const MethodChannel _channel =
-      const MethodChannel('native_http');
+const MethodChannel _channel = const MethodChannel('native_http');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+Future<NativeResponse> get(String url, {Map<String, dynamic> headers}) {
+  return _sendRequest(url: url, method: "POST", headers: headers);
+}
+
+Future<NativeResponse> post(String url,
+    {Map<String, dynamic> headers, Map<String, dynamic> body}) {
+  return _sendRequest(url: url, method: "POST", headers: headers, body: body);
+}
+
+Future<NativeResponse> _sendRequest({
+  String url,
+  String method,
+  Map<String, dynamic> headers = const {},
+  Map<String, dynamic> body = const {},
+}) async {
+  Map<String, dynamic> response =
+      await _channel.invokeMapMethod<String, dynamic>("native_http/post", {
+    "url": url,
+    "method": 'METHOD',
+    "headers": headers,
+    "body": body,
+  });
+  return NativeResponse._fromMap(response);
+}
+
+class NativeResponse {
+  int code;
+  String body;
+  dynamic getJson() => json.decode(body);
+  static NativeResponse _fromMap(Map<String, dynamic> response) {
+    return NativeResponse()
+      ..code = response["code"]
+      ..body = response["body"];
   }
 }
